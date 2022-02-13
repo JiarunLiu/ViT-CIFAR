2a873b32a57709384fa5a8b6c7562d3df9667496
JiarunLiu <jiarunliu@foxmail.com>
2022-02-13 12:45:39 +0800

    [ADD] vit-h training

---
+++ scripts/cifar10-noise-vit-h.sh
@@ -0,0 +1,14 @@
+python main.py --dataset c10 --noise-type clean --model-name vit-h --size 256 --label-smoothing --autoaugment
+
+python main.py --dataset c10 --noise-type clean --model-name vit-h --size 256
+
+# Symmetric noise
+for noise_rate in 0.2 0.4 0.6 0.8
+do
+  python main.py --dataset c10 --noise-type sn --noise-rate ${noise_rate} --model-name vit-h --size 256
+done
+# Pairflip noise
+for noise_rate in 0.2 0.45
+do
+  python main.py --dataset c10 --noise-type pairflip --noise-rate ${noise_rate} --model-name vit-h --size 256
+done
